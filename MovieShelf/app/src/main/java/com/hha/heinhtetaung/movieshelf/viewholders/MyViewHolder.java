6ea88df5e7331711b6336f7918f5f452994ff2e8
HeinHtetAung <14heinhtetaung@gmail.com>
2017-12-07 20:49:40 +0630

    Step1

---
+++ MovieShelf/app/src/main/java/com/hha/heinhtetaung/movieshelf/viewholders/MyViewHolder.java
@@ -0,0 +1,14 @@
+package com.hha.heinhtetaung.movieshelf.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by E5 on 12/7/2017.
+ */
+
+public class MyViewHolder extends RecyclerView.ViewHolder{
+    public MyViewHolder(View itemView) {
+        super(itemView);
+    }
+}
